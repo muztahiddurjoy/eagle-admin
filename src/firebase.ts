b86a1a56595cd3953936f7b5f80da2896382f17f
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-04-13 12:42:31 +0600

    firebase installed

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSi6CsEgAd4dPncaxMC8Ws5SHt5_VTtco",
+  authDomain: "eagle-security-bangladesh.firebaseapp.com",
+  projectId: "eagle-security-bangladesh",
+  storageBucket: "eagle-security-bangladesh.appspot.com",
+  messagingSenderId: "1076201514987",
+  appId: "1:1076201514987:web:8daf387fcb9db889531bae",
+  measurementId: "G-JH8K6EKGW6"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app)
